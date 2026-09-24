b8bea6f114c5a2e9f013d43a2aacd26db7450509
Italo <italo4dev@gmail.com>
2026-09-24 10:18:17 -0300

    feat: implementa seção nossa meta

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HeroSectionOuter from "./sections/heroSectionOuters/HeroSectionOuter";
 import CauseSectionOuter from "./sections/heroSectionCause/CauseSectionOuter";
 import FaqSectionOuter from "./sections/faqSection/FaqSectionOuter";
 import FacaDiferencaSection from "./sections/facaDiferencaSection/FacaDiferencaSection";
+import NossaMetaSection from "./sections/nossaMetaSection/NossaMetaSection";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <HeaderOuter />
       <HeroSectionOuter />
       <CauseSectionOuter />
+      <NossaMetaSection />
       <FacaDiferencaSection />
       <FaqSectionOuter />
     </div>
