6a36dc7bc58d39b292b2011d5a65b64756e09d77
Italo Ramos <77032241+italoandrezz@users.noreply.github.com>
2026-09-24 14:40:31 -0300

    Revert "Thaina/SectionCause (#23)" (#24)
    This reverts commit 76925db72acc1d411bfa2580f1423c5a7d346a5b.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import HeaderOuter from "./sections/header/HeaderOuter";
-import HeroSectionOuter from "./sections/heroSectionOuters/HeroSectionOuter";
-import CauseSectionOuter from "./sections/heroSectionCause/CauseSectionOuter";
-import FaqSectionOuter from "./sections/faqSection/FaqSectionOuter";
-import FacaDiferencaSection from "./sections/facaDiferencaSection/FacaDiferencaSection";
-import NossaMetaSection from "./sections/nossaMetaSection/NossaMetaSection";
-
 export default function App() {
   return (
     <div>
-      <HeaderOuter />
-      <main>
-        <HeroSectionOuter />
-        <CauseSectionOuter />
-        <NossaMetaSection />
-        <FacaDiferencaSection />
-        <FaqSectionOuter />
-      </main>
+      <h1>Meu Projeto</h1>
     </div>
   );
 }
